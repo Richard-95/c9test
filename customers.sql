81a7d35296f86bb6131cf840b079a8a0595ad28e
Richard-95 <richillwest@gmail.com>
2016-04-18 16:00:13 +0800

    Create customers.sql

---
+++ customers.sql
@@ -0,0 +1,85 @@
+create table customers (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	gender VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (1, 'Deborah', 'Brown', 'dbrown0@vistaprint.com', 'Female', '186.140.123.224');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (2, 'Jason', 'Kennedy', 'jkennedy1@yandex.ru', 'Male', '6.203.185.159');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (3, 'Sara', 'Diaz', 'sdiaz2@paginegialle.it', 'Female', '159.229.231.177');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (4, 'Clarence', 'Mendoza', 'cmendoza3@umn.edu', 'Male', '120.248.250.224');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (5, 'Sara', 'Fowler', 'sfowler4@msn.com', 'Female', '237.95.180.165');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (6, 'Emily', 'Payne', 'epayne5@rakuten.co.jp', 'Female', '108.38.142.55');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (7, 'Kenneth', 'Williamson', 'kwilliamson6@theguardian.com', 'Male', '46.50.248.125');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (8, 'Philip', 'Harris', 'pharris7@php.net', 'Male', '195.21.82.225');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (9, 'Alan', 'Adams', 'aadams8@wisc.edu', 'Male', '153.242.247.151');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (10, 'Richard', 'Gardner', 'rgardner9@dailymail.co.uk', 'Male', '232.49.6.23');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (11, 'Steven', 'Fernandez', 'sfernandeza@wikia.com', 'Male', '235.137.216.242');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (12, 'Patrick', 'Price', 'ppriceb@reddit.com', 'Male', '223.56.238.102');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (13, 'Debra', 'Johnston', 'djohnstonc@utexas.edu', 'Female', '48.253.160.19');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (14, 'Earl', 'Foster', 'efosterd@un.org', 'Male', '42.233.192.60');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (15, 'Sara', 'Murray', 'smurraye@yale.edu', 'Female', '174.223.7.2');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (16, 'Lori', 'Phillips', 'lphillipsf@bandcamp.com', 'Female', '87.91.242.9');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (17, 'Rachel', 'Perry', 'rperryg@blogtalkradio.com', 'Female', '194.50.165.43');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (18, 'Ernest', 'Arnold', 'earnoldh@last.fm', 'Male', '44.34.92.243');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (19, 'Theresa', 'Edwards', 'tedwardsi@eventbrite.com', 'Female', '241.57.65.179');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (20, 'Shawn', 'Lee', 'sleej@chron.com', 'Male', '29.0.18.22');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (21, 'Arthur', 'Long', 'alongk@howstuffworks.com', 'Male', '6.51.182.84');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (22, 'Joe', 'Kelley', 'jkelleyl@delicious.com', 'Male', '127.45.233.31');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (23, 'Kathleen', 'Castillo', 'kcastillom@xinhuanet.com', 'Female', '93.166.185.36');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (24, 'Arthur', 'Chavez', 'achavezn@creativecommons.org', 'Male', '100.104.190.143');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (25, 'Joyce', 'Cooper', 'jcoopero@google.it', 'Female', '79.73.234.203');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (26, 'Jason', 'Bell', 'jbellp@microsoft.com', 'Male', '86.63.243.159');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (27, 'Amy', 'Bishop', 'abishopq@businessweek.com', 'Female', '30.59.107.151');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (28, 'Jennifer', 'Ellis', 'jellisr@google.co.jp', 'Female', '109.105.226.131');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (29, 'Joseph', 'Gonzalez', 'jgonzalezs@state.tx.us', 'Male', '95.233.101.117');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (30, 'Mary', 'Bishop', 'mbishopt@infoseek.co.jp', 'Female', '171.248.210.21');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (31, 'Janice', 'Arnold', 'jarnoldu@slideshare.net', 'Female', '61.12.178.230');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (32, 'Robin', 'Carr', 'rcarrv@tripod.com', 'Female', '184.140.194.207');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (33, 'Bobby', 'Powell', 'bpowellw@statcounter.com', 'Male', '148.129.209.19');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (34, 'Kimberly', 'Rogers', 'krogersx@wordpress.com', 'Female', '245.181.230.90');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (35, 'Adam', 'Bryant', 'abryanty@sun.com', 'Male', '164.108.250.183');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (36, 'Ashley', 'Lynch', 'alynchz@dot.gov', 'Female', '160.244.0.219');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (37, 'Victor', 'Lewis', 'vlewis10@goo.ne.jp', 'Male', '33.48.14.53');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (38, 'Dorothy', 'Richardson', 'drichardson11@discovery.com', 'Female', '83.192.253.34');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (39, 'Jesse', 'Bryant', 'jbryant12@ning.com', 'Male', '60.190.152.64');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (40, 'Katherine', 'Simmons', 'ksimmons13@dion.ne.jp', 'Female', '222.77.204.116');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (41, 'Russell', 'Howell', 'rhowell14@ovh.net', 'Male', '100.192.204.97');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (42, 'Willie', 'Hart', 'whart15@vimeo.com', 'Male', '12.164.131.190');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (43, 'Roy', 'Walker', 'rwalker16@t-online.de', 'Male', '43.23.131.228');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (44, 'Dorothy', 'Perry', 'dperry17@vistaprint.com', 'Female', '144.136.18.104');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (45, 'Carl', 'Stevens', 'cstevens18@devhub.com', 'Male', '204.192.186.193');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (46, 'Julie', 'Reynolds', 'jreynolds19@washingtonpost.com', 'Female', '253.100.225.34');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (47, 'Bonnie', 'Peters', 'bpeters1a@springer.com', 'Female', '144.185.65.174');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (48, 'Stephanie', 'Parker', 'sparker1b@youtu.be', 'Female', '149.247.50.226');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (49, 'Antonio', 'Lane', 'alane1c@sun.com', 'Male', '4.202.228.98');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (50, 'Julia', 'Spencer', 'jspencer1d@china.com.cn', 'Female', '160.170.208.61');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (51, 'Teresa', 'Price', 'tprice1e@hatena.ne.jp', 'Female', '101.173.7.164');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (52, 'Pamela', 'Cooper', 'pcooper1f@wisc.edu', 'Female', '189.189.241.254');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (53, 'Denise', 'Matthews', 'dmatthews1g@qq.com', 'Female', '223.91.102.136');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (54, 'Julia', 'Reyes', 'jreyes1h@mashable.com', 'Female', '37.64.228.152');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (55, 'Shirley', 'Myers', 'smyers1i@fotki.com', 'Female', '22.49.58.240');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (56, 'Larry', 'Stanley', 'lstanley1j@java.com', 'Male', '74.185.30.186');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (57, 'Aaron', 'Simpson', 'asimpson1k@quantcast.com', 'Male', '89.87.23.200');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (58, 'Stephanie', 'Wallace', 'swallace1l@ucla.edu', 'Female', '49.114.156.243');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (59, 'Helen', 'Burke', 'hburke1m@slideshare.net', 'Female', '138.153.116.196');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (60, 'Robin', 'Duncan', 'rduncan1n@irs.gov', 'Female', '70.25.74.94');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (61, 'Carlos', 'Williamson', 'cwilliamson1o@blinklist.com', 'Male', '120.189.64.123');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (62, 'Kimberly', 'Riley', 'kriley1p@bbc.co.uk', 'Female', '35.145.56.131');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (63, 'Anthony', 'Kelley', 'akelley1q@harvard.edu', 'Male', '49.236.150.87');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (64, 'Beverly', 'Henry', 'bhenry1r@cmu.edu', 'Female', '110.4.5.9');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (65, 'Judy', 'Robertson', 'jrobertson1s@nbcnews.com', 'Female', '83.28.240.104');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (66, 'Larry', 'Reed', 'lreed1t@about.me', 'Male', '93.192.208.100');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (67, 'Raymond', 'Burns', 'rburns1u@dailymail.co.uk', 'Male', '46.203.199.122');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (68, 'Nancy', 'Clark', 'nclark1v@comcast.net', 'Female', '156.55.199.168');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (69, 'Sharon', 'Sullivan', 'ssullivan1w@reddit.com', 'Female', '33.217.153.15');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (70, 'Marie', 'Brown', 'mbrown1x@desdev.cn', 'Female', '87.237.205.97');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (71, 'Emily', 'Greene', 'egreene1y@chron.com', 'Female', '135.73.150.7');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (72, 'Doris', 'Alvarez', 'dalvarez1z@senate.gov', 'Female', '166.219.250.108');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (73, 'Lillian', 'Davis', 'ldavis20@yandex.ru', 'Female', '4.80.3.105');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (74, 'Henry', 'Cunningham', 'hcunningham21@istockphoto.com', 'Male', '123.162.130.61');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (75, 'Marie', 'Kennedy', 'mkennedy22@1688.com', 'Female', '50.181.144.173');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (76, 'Sandra', 'Gordon', 'sgordon23@printfriendly.com', 'Female', '5.192.141.149');
+insert into customers (id, first_name, last_name, email, gender, ip_address) values (77, 'Harold', 'Chavez', 'hchavez2...
